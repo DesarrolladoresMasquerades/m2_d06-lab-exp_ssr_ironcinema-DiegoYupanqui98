3af739fa6232af553f9a4ed4e1dddb3d497c68c1
Diego Yupanqui <diegoyupanquichavarry@gmail.com>
2022-02-14 00:26:48 +0100

    lab done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const Movie = require("../models/Movie.model")
+
+
+router.get('/movie/:id',(req,res,next)=>{
+
+    const id = req.params.id;
+    Movie.findById(id)
+    .then(
+        //(movie)=>console.log(movie)
+        (movie)=> res.render('details',movie)
+        
+        )
+
+})
+
+router.get('/movies',(req,res,next)=>{
+
+    Movie.find().then(movies=>
+        res.render("movies", {movies})
+        //console.log(movies)
+        )
+    });
+
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
 
